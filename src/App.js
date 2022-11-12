875b80aa1d09dda11726b6b349c78c18eac3b480
mekhtiyari <m.ekhtiyari1992@gmail.com>
2022-11-12 10:08:37 +0330

    first commit for pushing project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const fetchTours = async () => {
+    setLoading(true);
+    const respons = await fetch(url);
+    const tours = await respons.json();
+    console.log(tours);
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
+}
+
+export default App;
